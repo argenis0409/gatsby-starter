8a3c1fbe0a8c0af0bae2f152196bcb27763c5a40
Argenis De Los Santos <argenis04_09@icloud.com>
2020-05-04 13:12:46 -0700

    added styling to the started site

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import React from "react"
 
 export default () =>
 
-    <>
+    <div style={{ color: `purple` }}>
         <h1>Gatsby Starter Site</h1>
         <div >Hello world!</div>
-    </>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
